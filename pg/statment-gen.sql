24b3d1f0b1ef1a411be01fc655889bc4a5fa3301
wayne.yu <wayne.yu@altech.hk>
2024-03-04 16:50:42 +0800

    amend sql for extrqact statment record

--- pg/statment-gen.sql
+++ pg/statment-gen.sql
@@ -1,39 +1,47 @@
+-- export statement data
+-- 7143280938976280576 = brand_choice
+-- 7142615941312937984 = pcolle
 select
     p.id,
     p.amount,
     p.status,
     p.transaction_type,
-    p.metadata -> 'tenantContext' -> 'name' ->> 'en' as store_name,
+    p.metadata -> 'tenantContext' -> 'name' ->> 'en' as store,
     p.metadata ->> 'embosserName' as embosser_name,
     p.type,
     p.create_dt,
     p.update_dt
 from payment p
-where p.tenant_id = 7142615941312937984
-and p.create_dt >= '2024-01-01'
-and p.create_dt <= '2024-02-01'
+where p.tenant_id in
+(7143280938976280576)
+and p.create_dt >= '2024-02-15 15:00:00'
+and p.create_dt < '2024-02-29 15:00:00'
 ;
 
 
-
+-- Pcolle on 0201-0215
 SELECT
+    '2024-01-31 15:00:00' as startDt,
+    '2024-02-15 15:00:00' as endDt,
+    p.metadata -> 'tenantContext' -> 'name' ->> 'en' as store,
     tenant_id AS tenantId,
     transaction_type as transactionType,
-    metadata ->> 'cardBrand' AS cardBrand,
     currency,
     sum(amount) AS amount,
     status,
     COUNT(*) AS counter
 FROM payment p
-WHERE tenant_id = 7143280938976280576
-  AND transaction_type = 'SALES'
+WHERE tenant_id = 7142615941312937984
+  AND transaction_type in ('SALES')
   AND currency = 'JPY'
   AND mode = 'LIVE'
-  and p.create_dt >= '2024-01-31'
-  and p.create_dt <= '2024-02-15'
+  and p.create_dt >= '2024-01-31 15:00:00'
+  and p.create_dt < '2024-02-15 15:00:00'
+  AND status in ('COMPLETED', 'REFUNDED', 'REJECTED')
 GROUP BY
     tenant_id, transaction_type,
-    currency, cardBrand, status;
+    currency, status, store
+;
 
 
 SELECT
@@ -48,7 +56,7 @@ WHERE tenant_id = 7142615941312937984
   AND currency = 'JPY'
   AND mode = 'LIVE'
   and p.create_dt >= '2024-01-01'
-  and p.create_dt <= '2024-01-31'
+  and p.create_dt <= '2024-03-31';
 
 ------
 select
