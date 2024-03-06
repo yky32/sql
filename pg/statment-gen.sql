68b08fb28a9fe59c03b1fae0acd5cedbad08a796
wayne.yu <wayne.yu@altech.hk>
2024-03-06 20:15:10 +0800

    fix statement sql

--- pg/statment-gen.sql
+++ pg/statment-gen.sql
@@ -19,64 +19,74 @@ and p.create_dt < '2024-02-29 15:00:00'
 ;
 
 
--- Pcolle on 0201-0215
-SELECT
-    '2024-01-31 15:00:00' as startDt,
-    '2024-02-15 15:00:00' as endDt,
-    p.metadata -> 'tenantContext' -> 'name' ->> 'en' as store,
-    tenant_id AS tenantId,
-    transaction_type as transactionType,
-    currency,
-    sum(amount) AS amount,
-    status,
-    COUNT(*) AS counter
+-- ETL SQL: Pcolle on 0201-0215 ('SALES', 'REFUND')
+SELECT tenant_id                AS tenantId,
+       transaction_type         as transactionType,
+       p.metadata -> 'tenantContext' -> 'name' ->> 'en' as store,
+       metadata ->> 'cardBrand' AS cardBrand,
+       currency,
+       sum(amount)              AS amount,
+       status,
+       COUNT(*)                 AS counter
 FROM payment p
 WHERE tenant_id = 7142615941312937984
-  AND transaction_type in ('SALES')
-  AND currency = 'JPY'
+  AND transaction_type IN ('SALES', 'REFUND')
+  AND currency IN ('JPY')
   AND mode = 'LIVE'
-  and p.create_dt >= '2024-01-31 15:00:00'
-  and p.create_dt < '2024-02-15 15:00:00'
-  AND status in ('COMPLETED', 'REFUNDED', 'REJECTED')
-GROUP BY
-    tenant_id, transaction_type,
-    currency, status, store
+  AND p.create_dt >= '2024-01-31 15:00:00'
+  AND p.create_dt < '2024-02-15 15:00:00'
+GROUP BY tenant_id,
+         transaction_type,
+         currency,
+         cardBrand,
+         store,
+         status
 ;
 
-
-SELECT
-    tenant_id AS tenantId,
-    transaction_type as transactionType,
-    metadata ->> 'cardBrand' AS cardBrand,
-    currency,
-    status,
-    p.create_dt
+-- ETL SQL: Pcolle on 0216-0229
+SELECT tenant_id                AS tenantId,
+       transaction_type         as transactionType,
+       p.metadata -> 'tenantContext' -> 'name' ->> 'en' as store,
+       metadata ->> 'cardBrand' AS cardBrand,
+       currency,
+       sum(amount)              AS amount,
+       status,
+       COUNT(*)                 AS counter
 FROM payment p
 WHERE tenant_id = 7142615941312937984
-  AND currency = 'JPY'
+  AND transaction_type IN ('SALES', 'REFUND')
+  AND currency IN ('JPY')
   AND mode = 'LIVE'
-  and p.create_dt >= '2024-01-01'
-  and p.create_dt <= '2024-03-31';
+  AND p.create_dt >= '2024-02-15 15:00:00'
+  AND p.create_dt < '2024-02-29 15:00:00'
+GROUP BY tenant_id,
+         transaction_type,
+         currency,
+         cardBrand,
+         store,
+         status
+;
 
-------
-select
-    p.id,
-    p.hash,
-    p.amount,
-    p.mode,
-    p.status,
-    p.transaction_type,
-    p.metadata -> 'tenantContext' -> 'name' ->> 'en' as store_name,
-    p.metadata ->> 'embosserName' as embosser_name,
-    p.type,
-    p.create_dt,
-    p.update_dt
-from payment p
-WHERE tenant_id = 7142615941312937984
-  AND transaction_type = 'SALES'
-  AND currency = 'JPY'
+-- ETL SQL: Brand Choice on 0216-0229
+SELECT tenant_id                AS tenantId,
+       transaction_type         as transactionType,
+       p.metadata -> 'tenantContext' -> 'name' ->> 'en' as store,
+       metadata ->> 'cardBrand' AS cardBrand,
+       currency,
+       sum(amount)              AS amount,
+       status,
+       COUNT(*)                 AS counter
+FROM payment p
+WHERE tenant_id = 7143280938976280576
+  AND transaction_type IN ('SALES', 'REFUND')
+  AND currency IN ('JPY')
   AND mode = 'LIVE'
-  and p.create_dt >= '2023-12-31 15:00:00'
-  and p.create_dt <= '2024-01-31 15:00:00'
-  and p.status in ('REFUNDED')
+  AND p.create_dt >= '2024-02-15 15:00:00'
+  AND p.create_dt < '2024-02-29 15:00:00'
+GROUP BY tenant_id,
+         transaction_type,
+         currency,
+         cardBrand,
+         store,
+         status
 ;
