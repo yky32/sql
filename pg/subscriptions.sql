35d3dae7f616b0fa9939610cdca570be079fe77d
wayne.yu <wayne.yu@altech.hk>
2024-06-17 01:16:10 +0800

    add mend

--- pg/subscriptions.sql
+++ pg/subscriptions.sql
@@ -1,6 +1,3 @@
-SELECT pi.next_payment_dt, pi.previous_payment_dt,  pi.hash, pi.attempts, pi.index
-FROM payment_instruction pi;
-
 -- All pi related COS
 SELECT cs.hash, cs.ref_hash, cs.status
 FROM checkout_session cs
@@ -8,10 +5,27 @@ where ref_hash in (
     SELECT pi.hash
     FROM payment_instruction pi
     );
+-- PM
+-- pm_7208155155756220416 - 4242424242424241
+-- pm_7207212035531079680 - 4242424242424242
 
-SELECT cs.hash, cs.ref_hash, cs.status
-FROM checkout_session cs where type = 'SUBSCRIPTION';
+-- Step1. Valid Subscriptions
+SELECT
+    pi.status,
+    pi.payment_method_id,
+    pi.hash,
+    pi.end_dt,
+    pi.next_payment_dt,
+    pi.previous_payment_dt,pi.attempts, pi.index
+FROM payment_instruction pi;
 
+-- Step2. Generated COS
+SELECT cs.hash, cs.ref_hash, cs.status, cs.completed_dt
+FROM checkout_session cs where type = 'SUBSCRIPTION'
+ORDER BY cs.create_dt desc
+;
+
+-- Step3. Payment Details.
 SELECT p.status, p.hash, p.ref_hash
 FROM payment p
 WHERE type = 'SUBSCRIPTION_CHECKOUT'
