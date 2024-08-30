6dec2584fe964d393152da99dbf00acca11ac7eb
Wayne Yu <yky32is@gmail.com>
2024-08-30 14:30:20 +0800

    Update fdw.sql

--- pg/fdw.sql
+++ pg/fdw.sql
@@ -1,7 +1,46 @@
--- Install the FDW extension
-CREATE EXTENSION postgres_fdw;
+-- -- Step 1: Create the role
+-- CREATE ROLE local_user WITH LOGIN PASSWORD 'your_password';
+--
+-- -- Step 2: Grant privileges
+-- GRANT ALL PRIVILEGES ON DATABASE your_database TO local_user;
 
--- Create a foreign server for the other database
-CREATE SERVER foreign_log_db
+-- Step 3: Install the FDW extension
+-- CREATE EXTENSION postgres_fdw;
+
+-- Step 4: Create the foreign server
+CREATE SERVER tenant_foreign_server
     FOREIGN DATA WRAPPER postgres_fdw
-    OPTIONS (host 'pg-dev-do-user-14070403-0.b.db.ondigitalocean.com', dbname 'log', port '25060');
+    OPTIONS (host 'url', dbname 'tenant', port '25060');
+
+-- Step 5.0: Create admin user mapping
+CREATE USER MAPPING FOR doadmin
+    SERVER tenant_foreign_server
+    OPTIONS (user 'tenant', password '');
+
+-- Step 5: Create usage user mapping
+CREATE USER MAPPING FOR payment
+    SERVER tenant_foreign_server
+    OPTIONS (user 'tenant', password '');
+
+-- -- Step 6: Create the foreign table
+-- CREATE FOREIGN TABLE my_foreign_table (
+--     id integer,
+--     name text
+--     ) SERVER tenant_foreign_server
+--     OPTIONS (table_name 'public.remote_table');
+--
+-- -- Step 7: Query the foreign table
+-- SELECT * FROM my_foreign_table;
+
+-- Step 8: Import the foreign schemaa
+IMPORT FOREIGN SCHEMA public
+    LIMIT TO (tenant) -- [remote_table_name]
+    FROM SERVER tenant_foreign_server
+    INTO public; -- [my_schema]
+
+-- Step 8.1: Grant usage on the foreign server
+GRANT USAGE ON FOREIGN SERVER tenant_foreign_server TO payment;
+
+-- Step 8.2: Grant select privilege on the foreign table
+GRANT SELECT ON public.tenant -- [remote_table_name]
+    TO payment;
